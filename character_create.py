f179142b744ca467a7635f58d127ee77eff2e645
Joseph Macalinao <jmacalin@ix-dev.cs.uoregon.edu>
2023-01-22 14:37:36 -0800

    added fist lines of code

--- character_create.py
+++ character_create.py
@@ -1,15 +1,25 @@
-
-
-
-
-
-
-
-
-
-
-
+import time
+class Character:
+    def __init__(self, name, arch, hp, attack):
+        self.name = name
+        self.arch = arch
+        self.hp = hp
+        self.attack = attack
+        
 
 
 def main():
+    arch_choices = ["warrior", "wizard", "paladin", "berserk"]
+    name = input("What is your name: ")
+    time.sleep(.8)
+    print(f"Great to meet you {name}")
+    time.sleep(.5)
+    arch_decision = ""
+    while arch_decision.lower() not in arch_choices:
+        arch_decision = input("\n\n\nWhat type of character would you like to be:\nWarrior\nWizard\nPaladin\nBerserk\n")
+    character = Character(name, arch_decision, 10, 10)
+    print(character.attack)
+
     return 0
+
+main()
--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
+import time
 
 
 def main():
     # figure out where to start 
     # character creation?
-    print(8, 5)
+    print("Hello!")
+    time.sleep(2)
+    print("Welcome to Cave Dwellers, an adventure game made for up\nand coming adventurers like yourself!")
+    print("First things first, we need to get to know you a little bit.")
     return 0
 
 
