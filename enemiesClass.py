066f7c521797325b10bf8cc5a5b03fe44cfc665e
Joseph Macalinao <jhmac121101@gmail.com>
2023-02-02 11:20:35 -0800

    ignore

---
+++ enemiesClass.py
@@ -0,0 +1,23 @@
+class Goblin:
+
+    def __init__(self):
+        self.name = "Goblin"
+        self.health = 20
+        self.attack = 4
+
+
+class Orc:
+
+    def __init__(self):
+        self.name = "Orc"
+        self.health = 30
+        self.attack = 2
+
+
+class Wolf:
+
+    def __init__(self):
+        self.name = "Wolf"
+        self.health = 10
+        self.attack = 6
+
